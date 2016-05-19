9412a9d8e022c28dc4f8316a477b43f75318a021
Michael Dulle <michael.dulle@gmail.com>
2016-05-19 03:15:01 +0100

    Remove SSL stuff from base.sh

--- scripts/base.sh
+++ scripts/base.sh
@@ -23,27 +23,27 @@ sudo apt-get update
 sudo apt-get install -qq curl unzip git-core ack-grep software-properties-common build-essential cachefilesd
 
 
-echo ">>> Installing *.xip.io self-signed SSL"
+# echo ">>> Installing *.xip.io self-signed SSL"
 
-SSL_DIR="/etc/ssl/xip.io"
-DOMAIN="*.xip.io"
-PASSPHRASE="vaprobash"
+# SSL_DIR="/etc/ssl/xip.io"
+# DOMAIN="*.xip.io"
+# PASSPHRASE="vaprobash"
 
-SUBJ="
-C=US
-ST=Connecticut
-O=Vaprobash
-localityName=New Haven
-commonName=$DOMAIN
-organizationalUnitName=
-emailAddress=
-"
+# SUBJ="
+# C=US
+# ST=Connecticut
+# O=Vaprobash
+# localityName=New Haven
+# commonName=$DOMAIN
+# organizationalUnitName=
+# emailAddress=
+# "
 
-sudo mkdir -p "$SSL_DIR"
+# sudo mkdir -p "$SSL_DIR"
 
-sudo openssl genrsa -out "$SSL_DIR/xip.io.key" 1024
-sudo openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.csr" -passin pass:$PASSPHRASE
-sudo openssl x509 -req -days 365 -in "$SSL_DIR/xip.io.csr" -signkey "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.crt"
+# sudo openssl genrsa -out "$SSL_DIR/xip.io.key" 1024
+# sudo openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.csr" -passin pass:$PASSPHRASE
+# sudo openssl x509 -req -days 365 -in "$SSL_DIR/xip.io.csr" -signkey "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.crt"
 
 # Setting up Swap
 
